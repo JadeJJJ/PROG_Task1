22a6f81b511cc894767a82946dda7955673f4687
JayDWrld <101358812+JayDWrld@users.noreply.github.com>
2022-09-21 00:54:31 +0200

    Background

--- PROG_Task1/Replacing_Books.Designer.cs
+++ PROG_Task1/Replacing_Books.Designer.cs
@@ -30,6 +30,7 @@ namespace PROG_Task1
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Replacing_Books));
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
@@ -37,7 +38,6 @@ namespace PROG_Task1
             this.reset_btn = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.textbox1 = new System.Windows.Forms.Button();
             this.textbox2 = new System.Windows.Forms.Button();
@@ -52,14 +52,14 @@ namespace PROG_Task1
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(184, 31);
+            this.button1.Location = new System.Drawing.Point(245, 38);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 0;
             this.button1.Text = "Menu";
             this.button1.UseVisualStyleBackColor = true;
@@ -68,9 +68,10 @@ namespace PROG_Task1
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 30);
+            this.label1.Location = new System.Drawing.Point(10, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.Size = new System.Drawing.Size(73, 16);
             this.label1.TabIndex = 1;
             this.label1.Text = "00:00:00.00";
             // 
@@ -81,9 +82,10 @@ namespace PROG_Task1
             // 
             // start_btn
             // 
-            this.start_btn.Location = new System.Drawing.Point(3, 31);
+            this.start_btn.Location = new System.Drawing.Point(4, 38);
+            this.start_btn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.start_btn.Name = "start_btn";
-            this.start_btn.Size = new System.Drawing.Size(75, 23);
+            this.start_btn.Size = new System.Drawing.Size(100, 28);
             this.start_btn.TabIndex = 2;
             this.start_btn.Text = "START";
             this.start_btn.UseVisualStyleBackColor = true;
@@ -91,9 +93,10 @@ namespace PROG_Task1
             // 
             // reset_btn
             // 
-            this.reset_btn.Location = new System.Drawing.Point(96, 31);
+            this.reset_btn.Location = new System.Drawing.Point(128, 38);
+            this.reset_btn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.reset_btn.Name = "reset_btn";
-            this.reset_btn.Size = new System.Drawing.Size(75, 23);
+            this.reset_btn.Size = new System.Drawing.Size(100, 28);
             this.reset_btn.TabIndex = 4;
             this.reset_btn.Text = "RESET";
             this.reset_btn.UseVisualStyleBackColor = true;
@@ -101,51 +104,45 @@ namespace PROG_Task1
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.BackColor = System.Drawing.Color.Bisque;
             this.panel1.Controls.Add(this.start_btn);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.reset_btn);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel1.Location = new System.Drawing.Point(0, 565);
+            this.panel1.Location = new System.Drawing.Point(0, 695);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(980, 82);
+            this.panel1.Size = new System.Drawing.Size(1307, 101);
             this.panel1.TabIndex = 5;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 9);
+            this.label2.Location = new System.Drawing.Point(10, 23);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.Size = new System.Drawing.Size(43, 16);
             this.label2.TabIndex = 6;
             this.label2.Text = "Timer";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.label2);
-            this.panel2.Controls.Add(this.label1);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel2.Location = new System.Drawing.Point(890, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(90, 565);
-            this.panel2.TabIndex = 7;
-            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
-            // 
             // groupBox1
             // 
-            this.groupBox1.Location = new System.Drawing.Point(3, 80);
+            this.groupBox1.Location = new System.Drawing.Point(4, 98);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(582, 170);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(882, 209);
             this.groupBox1.TabIndex = 8;
             this.groupBox1.TabStop = false;
             // 
             // textbox1
             // 
             this.textbox1.BackColor = System.Drawing.Color.OrangeRed;
-            this.textbox1.Location = new System.Drawing.Point(10, 398);
+            this.textbox1.Location = new System.Drawing.Point(13, 490);
+            this.textbox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textbox1.Name = "textbox1";
-            this.textbox1.Size = new System.Drawing.Size(46, 133);
+            this.textbox1.Size = new System.Drawing.Size(61, 164);
             this.textbox1.TabIndex = 9;
             this.textbox1.Text = "Book1";
             this.textbox1.UseVisualStyleBackColor = false;
@@ -153,9 +150,10 @@ namespace PROG_Task1
             // textbox2
             // 
             this.textbox2.BackColor = System.Drawing.Color.DarkTurquoise;
-            this.textbox2.Location = new System.Drawing.Point(66, 398);
+            this.textbox2.Location = new System.Drawing.Point(88, 490);
+            this.textbox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textbox2.Name = "textbox2";
-            this.textbox2.Size = new System.Drawing.Size(46, 133);
+            this.textbox2.Size = new System.Drawing.Size(61, 164);
             this.textbox2.TabIndex = 10;
             this.textbox2.Text = "Book 2";
             this.textbox2.UseVisualStyleBackColor = false;
@@ -163,9 +161,10 @@ namespace PROG_Task1
             // textbox3
             // 
             this.textbox3.BackColor = System.Drawing.Color.Plum;
-            this.textbox3.Location = new System.Drawing.Point(124, 398);
+            this.textbox3.Location = new System.Drawing.Point(165, 490);
+            this.textbox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textbox3.Name = "textbox3";
-            this.textbox3.Size = new System.Drawing.Size(46, 133);
+            this.textbox3.Size = new System.Drawing.Size(61, 164);
             this.textbox3.TabIndex = 11;
             this.textbox3.Text = "Book 3";
             this.textbox3.UseVisualStyleBackColor = false;
@@ -173,9 +172,10 @@ namespace PROG_Task1
             // textbox4
             // 
             this.textbox4.BackColor = System.Drawing.Color.MediumSeaGreen;
-            this.textbox4.Location = new System.Drawing.Point(175, 398);
+            this.textbox4.Location = new System.Drawing.Point(233, 490);
+            this.textbox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textbox4.Name = "textbox4";
-            this.textbox4.Size = new System.Drawing.Size(46, 133);
+            this.textbox4.Size = new System.Drawing.Size(61, 164);
             this.textbox4.TabIndex = 12;
             this.textbox4.Text = "Book 4";
             this.textbox4.UseVisualStyleBackColor = false;
@@ -183,9 +183,10 @@ namespace PROG_Task1
             // textbox5
             // 
             this.textbox5.BackColor = System.Drawing.Color.SteelBlue;
-            this.textbox5.Location = new System.Drawing.Point(224, 398);
+            this.textbox5.Location = new System.Drawing.Point(299, 490);
+            this.textbox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textbox5.Name = "textbox5";
-            this.textbox5.Size = new System.Drawing.Size(46, 133);
+            this.textbox5.Size = new System.Drawing.Size(61, 164);
             this.textbox5.TabIndex = 13;
             this.textbox5.Text = "Book 5";
             this.textbox5.UseVisualStyleBackColor = false;
@@ -193,9 +194,10 @@ namespace PROG_Task1
             // textbox6
             // 
             this.textbox6.BackColor = System.Drawing.Color.HotPink;
-            this.textbox6.Location = new System.Drawing.Point(276, 398);
+            this.textbox6.Location = new System.Drawing.Point(368, 490);
+            this.textbox6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textbox6.Name = "textbox6";
-            this.textbox6.Size = new System.Drawing.Size(46, 133);
+            this.textbox6.Size = new System.Drawing.Size(61, 164);
             this.textbox6.TabIndex = 14;
             this.textbox6.Text = "Book 6";
             this.textbox6.UseVisualStyleBackColor = false;
@@ -203,9 +205,10 @@ namespace PROG_Task1
             // textbox7
             // 
             this.textbox7.BackColor = System.Drawing.Color.Salmon;
-            this.textbox7.Location = new System.Drawing.Point(328, 398);
+            this.textbox7.Location = new System.Drawing.Point(437, 490);
+            this.textbox7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textbox7.Name = "textbox7";
-            this.textbox7.Size = new System.Drawing.Size(46, 133);
+            this.textbox7.Size = new System.Drawing.Size(61, 164);
             this.textbox7.TabIndex = 15;
             this.textbox7.Text = "Book 7";
             this.textbox7.UseVisualStyleBackColor = false;
@@ -213,9 +216,10 @@ namespace PROG_Task1
             // textbox8
             // 
             this.textbox8.BackColor = System.Drawing.Color.Gold;
-            this.textbox8.Location = new System.Drawing.Point(381, 398);
+            this.textbox8.Location = new System.Drawing.Point(508, 490);
+            this.textbox8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textbox8.Name = "textbox8";
-            this.textbox8.Size = new System.Drawing.Size(46, 133);
+            this.textbox8.Size = new System.Drawing.Size(61, 164);
             this.textbox8.TabIndex = 16;
             this.textbox8.Text = "Book 8";
             this.textbox8.UseVisualStyleBackColor = false;
@@ -223,9 +227,10 @@ namespace PROG_Task1
             // textbox9
             // 
             this.textbox9.BackColor = System.Drawing.Color.Chartreuse;
-            this.textbox9.Location = new System.Drawing.Point(434, 398);
+            this.textbox9.Location = new System.Drawing.Point(579, 490);
+            this.textbox9.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textbox9.Name = "textbox9";
-            this.textbox9.Size = new System.Drawing.Size(46, 133);
+            this.textbox9.Size = new System.Drawing.Size(61, 164);
             this.textbox9.TabIndex = 17;
             this.textbox9.Text = "Book 9";
             this.textbox9.UseVisualStyleBackColor = false;
@@ -233,9 +238,10 @@ namespace PROG_Task1
             // textbox10
             // 
             this.textbox10.BackColor = System.Drawing.Color.LightBlue;
-            this.textbox10.Location = new System.Drawing.Point(486, 398);
+            this.textbox10.Location = new System.Drawing.Point(648, 490);
+            this.textbox10.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textbox10.Name = "textbox10";
-            this.textbox10.Size = new System.Drawing.Size(46, 133);
+            this.textbox10.Size = new System.Drawing.Size(61, 164);
             this.textbox10.TabIndex = 18;
             this.textbox10.Text = "Book 10";
             this.textbox10.UseVisualStyleBackColor = false;
@@ -244,8 +250,7 @@ namespace PROG_Task1
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(316, 301);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(421, 370);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(208, 31);
             this.label3.TabIndex = 19;
@@ -255,8 +260,7 @@ namespace PROG_Task1
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(321, 30);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(428, 37);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(177, 31);
             this.label4.TabIndex = 20;
@@ -264,11 +268,14 @@ namespace PROG_Task1
             // 
             // Replacing_Books
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Bisque;
-            this.ClientSize = new System.Drawing.Size(980, 647);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1307, 796);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textbox10);
             this.Controls.Add(this.textbox9);
@@ -281,14 +288,12 @@ namespace PROG_Task1
             this.Controls.Add(this.textbox2);
             this.Controls.Add(this.textbox1);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Replacing_Books";
             this.Text = "Replacing_Books";
             this.Load += new System.EventHandler(this.Replacing_Books_Load);
             this.panel1.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -303,7 +308,6 @@ namespace PROG_Task1
         private System.Windows.Forms.Button reset_btn;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button textbox1;
         private System.Windows.Forms.Button textbox2;
