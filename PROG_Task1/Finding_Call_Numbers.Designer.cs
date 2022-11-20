480e0effbc6594a71f895a2733bdb2eb9d3cefed
JayDWrld <101358812+JayDWrld@users.noreply.github.com>
2022-11-20 20:02:26 +0200

    Colour

--- PROG_Task1/Finding_Call_Numbers.Designer.cs
+++ PROG_Task1/Finding_Call_Numbers.Designer.cs
@@ -49,6 +49,7 @@ namespace PROG_Task1
             // 
             // panel1
             // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.button2);
@@ -131,52 +132,62 @@ namespace PROG_Task1
             // 
             // Question
             // 
-            this.Question.Location = new System.Drawing.Point(80, 110);
+            this.Question.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Question.Location = new System.Drawing.Point(25, 110);
             this.Question.Name = "Question";
-            this.Question.Size = new System.Drawing.Size(100, 23);
+            this.Question.Size = new System.Drawing.Size(253, 54);
             this.Question.TabIndex = 1;
-            this.Question.Text = "label3";
+            this.Question.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // o1
             // 
+            this.o1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.o1.Location = new System.Drawing.Point(625, 85);
             this.o1.Name = "o1";
             this.o1.Size = new System.Drawing.Size(296, 47);
             this.o1.TabIndex = 2;
             this.o1.Text = "label3";
+            this.o1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.o1.Click += new System.EventHandler(this.o1_Click);
             // 
             // o2
             // 
+            this.o2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.o2.Location = new System.Drawing.Point(625, 156);
             this.o2.Name = "o2";
             this.o2.Size = new System.Drawing.Size(296, 45);
             this.o2.TabIndex = 3;
             this.o2.Text = "label4";
+            this.o2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.o2.Click += new System.EventHandler(this.o2_Click);
             // 
             // o3
             // 
+            this.o3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.o3.Location = new System.Drawing.Point(625, 222);
             this.o3.Name = "o3";
             this.o3.Size = new System.Drawing.Size(296, 45);
             this.o3.TabIndex = 4;
             this.o3.Text = "label5";
+            this.o3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.o3.Click += new System.EventHandler(this.o3_Click);
             // 
             // o4
             // 
+            this.o4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.o4.Location = new System.Drawing.Point(625, 304);
             this.o4.Name = "o4";
             this.o4.Size = new System.Drawing.Size(296, 45);
             this.o4.TabIndex = 5;
             this.o4.Text = "label6";
+            this.o4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.o4.Click += new System.EventHandler(this.o4_Click);
             // 
             // Finding_Call_Numbers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(933, 542);
             this.Controls.Add(this.o4);
             this.Controls.Add(this.o3);
